da2dcc3ae6f52125289153dc179f4f1a5a878c1a
Zetsuy <jeremygarciamobile@gmail.com>
2022-04-29 10:05:17 +0200

    Test addition lot of number

--- app/js/stringCalculator.js
+++ app/js/stringCalculator.js
@@ -2,13 +2,35 @@ export class stringCalculator {
 
     add(str) {
 
-        if(str == ""){
-            return 0;            
-        }
-    
-        
-    }
-    
-  }
+        if (str == "") {
 
-  
+            return 0;
+        }
+        else {
+
+            let nums = [];
+            let addition = 0;
+
+            for (let i = 0; i < str.length; i++) {
+
+                if (!isNaN(Number(str[i]))) {
+
+                    nums.push(Number(str[i]));
+
+                }
+
+            }
+
+            for (let i = 0; i < nums.length; i++) {
+
+                addition += nums[i];
+
+            }
+
+            return addition;
+
+        }
+
+    }
+}
+
--- app/tests/stringCalculator.test.js
+++ app/tests/stringCalculator.test.js
@@ -11,4 +11,34 @@ describe('String Calculator', () => {
 
     });
 
+    test('for string 1 it returns 1', () => {    
+
+        expect(Calculator.add("1")).toBe(1);
+    
+    });
+
+    test('for string 1 it not returns 2', () => {    
+
+        expect(Calculator.add("1")).not.toBe(2);
+    
+    });
+
+    test('for string 1,2 it returns 3', () => {    
+
+        expect(Calculator.add("1,2")).toBe(3);
+    
+    });
+
+    test('for string 1,2,3,4,5 it returns 15', () => {    
+
+        expect(Calculator.add("1,2,3,4,5")).toBe(15);
+    
+    });
+
+    test('for string 1\n2,3 it returns 6', () => {    
+
+        expect(Calculator.add("1\n2,3")).toBe(6);
+    
+    });
+
 });
